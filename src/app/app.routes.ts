1e2a9e37fa82a2df10e9277c2c39fea80e19f4dc
Ze Franco <josepedrompfranco@gmail.com>
2026-01-29 20:18:02 +0000

    Header and pages created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home';
+import { ShopsComponent } from './shops/shops';
+import { AboutComponent } from './about/about';
+import { ContactsComponent } from './contacts/contacts';
+import { LoginComponent } from './login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },              // Página inicial (vazio)
+  { path: 'shops', component: ShopsComponent }, // localhost:4200/barbeiros
+  { path: 'about', component: AboutComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'login', component: LoginComponent }
+];
